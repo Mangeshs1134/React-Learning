bf627ac8145c254971f166f8b54275c19d1a124b
Mangesh Gupta <mangeshs1134@gmail.com>
2023-10-12 23:16:52 +0530

    Add existing file

---
+++ src/NewComp.jsx
@@ -0,0 +1,9 @@
+function NewApp() {
+    return (
+    <h2>
+        A learner , a curious person.
+    </h2>
+    )
+  }
+  
+  export default NewApp
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import NewApp from './NewComp.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <NewApp />
   </React.StrictMode>,
 )
